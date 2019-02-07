1996ca1d1b3291f1bb3b48f6556ae1af9e83afb1
Alex-V-Kovalev <a.kovalev@mail.ru>
2019-02-07 14:44:51 +0300

    Доп задания 1.4.10

---
+++ src/lesson_02/additionaltasks/Deal.java
@@ -0,0 +1,79 @@
+package lesson_02.additionaltasks;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deal {
+    public static void main(String[] args) {
+        int deal;
+        try {
+            deal = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.err.println("Неверный аргумент вызова программы\n" +
+                    "Попробуйте: Deal <целое_число_раздач>");
+            return;
+        }
+
+        String[] cards = new String[52];
+
+        deckInit(cards);
+
+        System.out.println(Arrays.toString(cards));
+
+        deckSuffle(cards);
+
+        System.out.println(Arrays.toString(cards));
+
+        System.out.println("\nНачинаем раздачу:");
+        int index = 0;
+        for (int i = 0; i < deal; i++) {
+            System.out.print("" + (i + 1) + " рука: ");
+            for (int j = 0; j < 5; j++) {
+                String delim;
+                delim = j < 4 ? ", " : "";
+                System.out.print(cards[index++] + delim);
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    private static void deckInit(String[] cards) {
+        for (int i = 0; i < cards.length; i++) {
+            String vol = "";
+            switch (i / 13) {
+                case 0:
+                    vol = "♥️";
+                    break;
+                case 1:
+                    vol = "♣️";
+                    break;
+                case 2:
+                    vol = "♦️";
+                    break;
+                case 3:
+                    vol = "♠️";
+                    break;
+            }
+
+            if (i % 13 < 9) vol += (i % 13 + 2);
+            if (i % 13 == 9) vol += "В";
+            if (i % 13 == 10) vol += "Д";
+            if (i % 13 == 11) vol += "К";
+            if (i % 13 == 12) vol += "Т";
+
+            cards[i] = vol;
+        }
+    }
+
+    public static void deckSuffle(String[] cards) {
+        int count = cards.length;
+        for (int i = 0; i < count - 1; i++) {
+            int rnd = (int) (Math.random() * (count - i) + i);
+            String tmp = cards[rnd];
+            cards[rnd] = cards[i];
+            cards[i] = tmp;
+        }
+    }
+}
