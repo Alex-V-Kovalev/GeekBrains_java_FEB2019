22d13e270fe5610161e925b501750ab49b99a3c9
Alex-V-Kovalev <a.kovalev@mail.ru>
2019-02-07 16:24:16 +0300

    Доп. задание 1.4.11

---
+++ src/lesson_02/additionaltasks/HowMany.java
@@ -0,0 +1,7 @@
+package lesson_02.additionaltasks;
+
+public class HowMany {
+    public static void main(String[] args) {
+        System.out.println("Количество переданных аргументов программе: " + args.length);
+    }
+}
